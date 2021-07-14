95242076918bdb0a00bf347327c597ebc2ea9854
TomatoSoupSoup <>
2021-07-14 13:07:08 +0000

    created a app.js fill

---
+++ app.js
@@ -0,0 +1,23 @@
+alert("Welcome summoner")
+var name = prompt("what name do you go by?") 
+alert(name + " huh, never heard of it" )
+alert("And what kind of heros are you "+ name + " ?") 
+
+
+
+var character = prompt("would you rather be: \n a-Fast and stealthy \n b-Strong and tanky \n c-healing and support \n(answer with alphabites)" )
+
+if(character == 'a')
+alert("so you are an assassin as i can see")
+
+
+
+else if (character == 'b'){
+alert("so ur a fighter even though u dont look like one")
+}
+
+else if (character == 'c'){
+alert("Healer huh? it would be nice if someone finally healed my back pain")
+}
+ document.write(enjoy ur stay traveler)
+
